2e68559eef6dc19151fb1ecfc9a202bb53cd2587
swarthyPig <38877902+swarthyPig@users.noreply.github.com>
2018-10-29 22:37:03 +0900

    Add files via upload

--- DHT22WeMos.ino
+++ DHT22WeMos.ino
@@ -1,40 +1,29 @@
-// Including the ESP8266 WiFi library
 #include <ESP8266WiFi.h>
 #include "DHT.h"
+#define DHTTYPE DHT22
 
-// Uncomment one of the lines below for whatever DHT sensor type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-
-// Replace with your network details
 const char* ssid = "BlackPig";
 const char* password = "dy**909090";
 //const char* ssid = "chaoslab1";
 //const char* password = "biochaos";
 
-// Web Server on port 80
 WiFiServer server(80);
 
-// DHT Sensor
+
 const int DHTPin = 2; // weMos 핀맵에따라 D4에 연결한다
-// Initialize DHT sensor.
+
 DHT dht(DHTPin, DHTTYPE);
 
-// Temporary variables
 static char celsiusTemp[7];
 static char fahrenheitTemp[7];
 static char humidityTemp[7];
 
-// only runs once on boot
 void setup() {
-  // Initializing serial port for debugging purposes
   Serial.begin(115200);
   delay(10);
 
   dht.begin();
   
-  // Connecting to WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -49,50 +38,41 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
   
-  // Starting the web server
   server.begin();
   Serial.println("Web server running. Waiting for the ESP IP...");
   delay(10000);
   
-  // Printing the ESP IP address
   Serial.println(WiFi.localIP());
 }
 
-// runs over and over again
 void loop() {
-  // Listenning for new clients
   WiFiClient client = server.available();
   
   if (client) {
     Serial.println("New client");
-    // bolean to locate when the http request ends
+
     boolean blank_line = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
         
         if (c == '\n' && blank_line) {
-            // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+           
             float h = dht.readHumidity();
-            // Read temperature as Celsius (the default)
             float t = dht.readTemperature();
-            // Read temperature as Fahrenheit (isFahrenheit = true)
             float f = dht.readTemperature(true);
-            // Check if any reads failed and exit early (to try again).
+           
             if (isnan(h) || isnan(t) || isnan(f)) {
               Serial.println("Failed to read from DHT sensor!");
               strcpy(celsiusTemp,"Failed");
               strcpy(fahrenheitTemp, "Failed");
               strcpy(humidityTemp, "Failed");         
             }
-            else{
-              // Computes temperature values in Celsius + Fahrenheit and Humidity
-              //float hic = dht.computeHeatIndex(t, h, false);       
-              dtostrf(t, 6, 2, celsiusTemp);             
-              //float hif = dht.computeHeatIndex(f, h);
+            else{  
+              dtostrf(t, 6, 2, celsiusTemp);   
               dtostrf(f, 6, 2, fahrenheitTemp);         
               dtostrf(h, 6, 2, humidityTemp);
-              // You can delete the following Serial.print's, it's just for debugging purposes
+              
               Serial.print("Humidity: ");
               Serial.print(h);
               Serial.print(" %\t Temperature: ");
@@ -106,7 +86,7 @@ void loop() {
             client.println("Connection: keep-alive");
             client.println("Refresh: 2");
             client.println();
-            // your actual web page that displays temperature and humidity
+ 
             client.println("<!DOCTYPE HTML>");
             client.println("<html>");
             client.println("<head></head>");
@@ -126,16 +106,13 @@ void loop() {
             break;
         }
         if (c == '\n') {
-          // when starts reading a new line
           blank_line = true;
         }
         else if (c != '\r') {
-          // when finds a character on the current line
           blank_line = false;
         }
       }
     }  
-    // closing the client connection
     delay(1);
     client.stop();
     Serial.println("Client disconnected.");
